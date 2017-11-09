b824a4aa10d441d8d8c6f0e0b19b035dba7d211e
ashishcha <ashish.chandrah@gmail.com>
2017-11-09 11:37:34 -0600

    Test Commit

---
+++ simp1.py
@@ -0,0 +1,11 @@
+from github import Github
+
+g = Github("ashishcha", "hsihsa@17")
+
+for repo in g.get_user().get_repos():
+    print repo.full_name
+
+
+repo = g.get_user().get_repo("Hello-World")
+file =  repo.get_file_contents("gcd.c")
+
